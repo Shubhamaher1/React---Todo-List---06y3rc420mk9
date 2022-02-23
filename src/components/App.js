b356d4804ffe49a5e4d3cc542fe1dbfe94dc8a68
Shubham  Aher <84302530+Shubhamaher1@users.noreply.github.com>
2022-02-23 12:17:20 +0530

    to

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import "./../styles/App.css";
 
-function App() 
-{
-	return (
-	<div id="main">
-	//Do not alter main div
-	//Please do not alter the functional component as tests depend on the type of component.
-	</div>
-	);
-}
+function App() {
+  const [value, setValue] = useState("");
+  const [list, setList] = useState([]);
 
+  return (
+    <div id="main">
+      <textarea
+        id="task"
+        type="text"
+        value={value}
+        onChange={(e) => {
+          setValue(e.target.value);
+        }}
+      ></textarea>
+      <button
+        id="btn"
+        onClick={() => {
+          setList((list) => [...list, value]);
+          setValue("");
+        }}
+      >
+        {" "}
+        Add
+      </button>
+      <div>
+        {list.map((ele, index) => (
+          <>
+            <div className="list">{ele}</div>
+            <button>delet</button>
+            <button>edit</button>
+          </>
+        ))}
+      </div>
+    </div>
+  );
+}
 
 export default App;
